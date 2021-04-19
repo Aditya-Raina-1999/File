9c908b3cddaff5f8c5cedd6efddf68a3236a0723
Aditya-Raina-1999 <adityaraina611999@gmail.com>
2021-04-19 13:42:00 +0530

    New branch is updated

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 app.get("/",function(req,res){
-  res.send("Home Page");
-  console.log("Home Page");
+  res.send("Page");
+  console.log("Page");
 });
 app.listen(3000,function(){
-  console.log("The server is running");
+  console.log("Server is running fine");
 });
